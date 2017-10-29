509bd07ad98ce8fcdc5e036955065d95ddd12d17
jul-pro <jul-pro@ukr.net>
2017-10-29 10:21:47 +0300

    red errors

--- js/validation.js
+++ js/validation.js
@@ -1,4 +1,18 @@
 $(function() {
+
+	$.validator.setDefaults({
+		errorClass: "help-block",
+		highlight: function(element) {
+			$(element)
+				.closest(".form-group")
+				.addClass("has-error");
+		},
+		unhighlight: function(element) {
+			$(element)
+				.closest(".form-group")
+				.removeClass("has-error");
+		}
+	});
 	
 
 	$("#message_form").validate( {
